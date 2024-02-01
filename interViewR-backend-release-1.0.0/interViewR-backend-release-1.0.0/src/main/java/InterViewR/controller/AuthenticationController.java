10d83087de696f41d0a5322285116a0864ec6931
Dragos Ciota <dragosciota2002@gmail.com>
2024-02-01 12:13:36 +0200

    Backend

---
+++ interViewR-backend-release-1.0.0/interViewR-backend-release-1.0.0/src/main/java/InterViewR/controller/AuthenticationController.java
@@ -0,0 +1,27 @@
+package InterViewR.controller;
+
+import InterViewR.requests.SignInRequest;
+import InterViewR.requests.SignUpRequest;
+import InterViewR.responses.JwtAuthenticationResponse;
+import InterViewR.security.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@CrossOrigin
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
